e18dd572d3bac21f85d1af93a3045081004a95e4
prioto <hailprioto@gmail.com>
2023-12-13 23:59:32 -0500

    cambiar ruta

--- src/resources/fetch.ts
+++ src/resources/fetch.ts
@@ -1,4 +1,5 @@
-const API_URL = process.env.REACT_APP_API_URL;
+// const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = "https://sistema-electoral.onrender.com";
 const headers = {
     "Content-Type": "application/json"
 }
